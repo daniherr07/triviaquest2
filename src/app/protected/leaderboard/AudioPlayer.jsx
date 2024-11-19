ab699b6bafa0d81fa88a192305361b5f737b0f2c
maindream <dherrera2195@gmail.com>
2024-11-18 22:33:39 -0600

    changes

---
+++ src/app/protected/leaderboard/AudioPlayer.jsx
@@ -0,0 +1,8 @@
+'use client'
+
+export default function AudioPlayer(){
+    return      <audio autoPlay loop>
+    <source src="/lobby.mp3" type="audio/mpeg" />
+    Your browser does not support the audio element.
+</audio>
+}
--- src/app/protected/leaderboard/page.jsx
+++ src/app/protected/leaderboard/page.jsx
@@ -1,13 +1,24 @@
 import styles from "./styles.module.css";
-import { cookies } from 'next/headers'
 import AudioPlayer from "./AudioPlayer";
+import { address } from "@/app/const";
+import { cookies } from "next/headers";
   
-  
-export default async function Play() {
+async function getData() {
+  const res = await fetch(`${address}/getLeaderboard`)
   const cookieStore = await cookies()
   const authCookie = cookieStore.get('auth')
   const userData = JSON.parse(authCookie.value)
-
+  
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+  
+export default async function Play() {
+    const users = await getData()
+    console.log(users)
   
     return (
       
@@ -21,12 +32,26 @@ export default async function Play() {
         <div className={`${styles.cajita5} ${styles.cajita}`} />
       </div>
       <main className={styles.main}>
-        <h1 className={styles.headerH1}>TriviaQuest</h1>
-        <h2 className={styles.text}>Hola de nuevo <span className={styles.userName}>{userData.userName}</span></h2>
-        <button type="button" className={styles.play} ><a href="/protected/trivia">Jugar</a></button>
-        <h2 className={styles.text}>Puntaje maximo: {userData.high}</h2>
-        <button type="button" className={styles.faq}><a href="/protected/leaderboard">Leaderboard</a></button>
-        <button type="button" className={styles.faq}><a href="/api/logout">Cerrar Sesion</a></button>
+        <h1 className={styles.headerH1}>Leaderboard</h1>
+        <div className={styles.tableWrapper}>
+          <table className={styles.table}>
+            <thead className={styles.tableHead}>
+              <tr>
+                <th>Nombre</th>
+                <th>Puntaje</th>
+              </tr>
+            </thead>
+            <tbody className={styles.tableBody}>
+              {users.map((user, index) => (
+                <tr key={index}>
+                  <td>{user.username} </td>
+                  <td>{user.highScore}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <button type="button" className={styles.play} ><a href="/protected/play">Regresar</a></button>
       </main>
 
 
