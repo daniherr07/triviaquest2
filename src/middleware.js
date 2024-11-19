46cc0d194e6bdfc91d3103f01915a9046e088915
maindream <dherrera2195@gmail.com>
2024-11-18 21:44:34 -0600

    login and middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,13 +4,21 @@ export function middleware(request) {
     // Verificar si la cookie "auth" existe
     const authCookie = request.cookies.get('auth');
     
-    // Si la cookie no existe, redirige a la página de login
-    if (!authCookie) {
+    // Si estamos en la ruta raíz y la cookie existe, redirigir al dashboard
+    if (request.nextUrl.pathname === '/' && authCookie) {
+        return NextResponse.redirect(new URL('/protected/play', request.url));
+    }
+    
+    // Si estamos en una ruta protegida y la cookie no existe, redirigir a la página de login
+    if (request.nextUrl.pathname.startsWith('/protected') && !authCookie) {
         return NextResponse.redirect(new URL('/', request.url));
     }
+
+    // En cualquier otro caso, permitir la solicitud
+    return NextResponse.next();
 }
 
-// Aplica el middleware solo en la ruta de 'protected'
+// Aplica el middleware en la ruta raíz y en las rutas protegidas
 export const config = {
-    matcher: '/protected/:path*',
+    matcher: ['/', '/protected/:path*'],
 };
